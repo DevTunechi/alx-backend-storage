aef470bbf24fdc4e7013ee4fc48a04eb88fdb1d8
Olatunji Oluwadare <tunjidare2@yahoo.com>
2024-08-15 19:05:39 +0100

    Create 12-log_stats.py

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+"""
+This module contains a function that displays stats about Nginx logs stored
+in MongoDB
+"""
+
+from pymongo import MongoClient
+
+
+def display_log_stats():
+    """
+    Display statistics of different HTTP methods in the nginx logs
+    """
+
+    client = MongoClient()
+    nginx = client.logs.nginx
+
+    print(nginx.count_documents({}), "logs")
+    print("Methods:")
+
+    for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
+        print(
+            f"\tmethod {method}: {nginx.count_documents({'method': method})}"
+        )
+
+    print(f"{nginx.count_documents({'path': '/status'})} status check")
+
+
+if __name__ == "__main__":
+    display_log_stats()
